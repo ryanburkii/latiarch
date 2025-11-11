244dc7def48dca7348d46488a5cccf35a129bba4
ryanburkii <73972839+ryanburkii@users.noreply.github.com>
2025-11-11 10:52:40 -0500

    Update config.sh

--- install/config/config.sh
+++ install/config/config.sh
@@ -14,16 +14,6 @@ cp ~/.local/share/latiarch/default/zshrc ~/.zshrc
 # Change default shell to zsh if it's not already
 shell_path="$(command -v zsh || echo /usr/bin/zsh)"
 
-if ! grep -Fxq "$shell_path" /etc/shells; then
-  echo "$shell_path" | sudo tee -a /etc/shells >/dev/null
-fi
-
-current_shell="$(getent passwd "$USER" | cut -d: -f7)"
-
-if [[ "$current_shell" != "$shell_path" ]]; then
-  if [[ -n ${LATIARCH_CHROOT_INSTALL:-} ]]; then
-    sudo chsh --root /mnt -s "$shell_path" "$USER"
-  else
-    sudo chsh -s "$shell_path" "$USER"
-  fi
+if [[ -x "$shell_path" ]]; then
+  sudo chsh -s "$shell_path" "$USER"
 fi
