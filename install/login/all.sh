627028c3c0fe546241165a99998c902e2429fc7f
ryanburkii <73972839+ryanburkii@users.noreply.github.com>
2025-11-11 13:09:57 -0500

    fixes

--- install/login/all.sh
+++ install/login/all.sh
@@ -2,5 +2,4 @@
 
 run_logged $LATIARCH_INSTALL/login/plymouth.sh
 run_logged $LATIARCH_INSTALL/login/limine-snapper.sh
-run_logged $LATIARCH_INSTALL/login/enable-mkinitcpio.sh
-run_logged $LATIARCH_INSTALL/login/alt-bootloaders.sh
+run_logged $LATIARCH_INSTALL/login/sddm.sh
